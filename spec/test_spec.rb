bbb68828c891657ab1b89715846027396515caf0
caleb.teo <caleb.teo@workday.com>
2019-02-12 10:03:51 +0000

    Changes to be committed: 	modified:   spec/test_spec.rb 	Create test to fail pipeline

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -5,6 +5,6 @@ describe 'test_spec' do
   it 'test_count' do
       counter = Student.new('caleb',10, 'Eng')
       count = Student.no_student()
-      expect(count).to eq 4
+      expect(count).to eq 3
   end
 end
